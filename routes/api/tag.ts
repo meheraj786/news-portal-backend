fc7c63e359c39626f99c9d3ec297ae7b17b44ebe
sanvir-islam <sanvirofficials@gmail.com>
2025-12-23 10:43:22 +0600

    remove the unused tag controller and route

--- routes/api/tag.ts
+++ routes/api/tag.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { getAllTags, getPostsByTag } from "../../controllers/tagController";
+// import express from "express";
+// import { getAllTags, getPostsByTag } from "../../controllers/tagController";
 
-const tagRoutes = express.Router();
+// const tagRoutes = express.Router();
 
-// 1. Get All Tags (Returns tag list with post counts)
-// URL: GET /api/tags
-tagRoutes.get("/", getAllTags);
+// // 1. Get All Tags (Returns tag list with post counts)
+// // URL: GET /api/tags
+// tagRoutes.get("/", getAllTags);
 
-// 2. Get Posts by Tag Name
-// URL: GET /api/tags/posts?tagName=politics&page=1
-tagRoutes.get("/posts", getPostsByTag);
+// // 2. Get Posts by Tag Name
+// // URL: GET /api/tags/posts?tagName=politics&page=1
+// tagRoutes.get("/posts", getPostsByTag);
 
-export default tagRoutes;
+// export default tagRoutes;
